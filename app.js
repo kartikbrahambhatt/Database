8ca042b2e76ea8c27a3866fe1b6f027f21627340
kartik brahambhatt <kartikbrahambhatt08@gmail.com>
2025-08-13 16:38:31 +0530

    bro

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ app.get('/', (req, res) => {
   res.send('Hello from backend');
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
